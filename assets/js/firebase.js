5227c248f0290353f26f63716faa321522314dc1
Bamidele Emmanuel <emmynuel02@gmail.com>
2023-04-19 15:44:28 +0100

    Initial commit

---
+++ assets/js/firebase.js
@@ -0,0 +1,15 @@
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCIefnpD5hsJndlUK95Y9fVHhqRztR5XDI",
+    authDomain: "coretneedle-web.firebaseapp.com",
+    projectId: "coretneedle-web",
+    storageBucket: "coretneedle-web.appspot.com",
+    messagingSenderId: "322882617129",
+    appId: "1:322882617129:web:039a1109f8d321be464138",
+    measurementId: "G-6QXZL4X904"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
